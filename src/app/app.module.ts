059812ef8dc1345b0459b85a68ffad967f6ead1f
kote1999 <kotekirkitadze@gmail.com>
2021-11-14 14:44:39 +0400

    some refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,14 @@ import { AppComponent } from './app.component';
 import { COUNTRY_API, TIMELINE_API } from './services/data-api.service';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { TimelineComponent } from './components/timeline/timeline.component';
-import { CalendarModule } from 'primeng/calendar';
 import { FormsModule } from '@angular/forms';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { TableModule } from 'primeng/table';
 import { NotFoundComponent } from './shell/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './shared/material.module';
+import { ViewComponentsModule } from './shared/material.module';
 import { HeaderComponent } from './shell/header/header.component';
 import { VisualizationCharComponentComponent } from './components/timeline/visualization-char-component/visualization-char-component.component';
 import { CountryInfoShellComponent } from './components/country-info-shell/country-info-shell.component';
 import { TableComponent } from './components/country-info-shell/table/table.component';
-import { ListboxModule } from 'primeng/listbox';
-import { DropdownModule } from 'primeng/dropdown';
 
 
 
@@ -39,19 +34,13 @@ import { DropdownModule } from 'primeng/dropdown';
   imports: [
     BrowserModule,
     HttpClientModule,
-
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
     }),
     BrowserAnimationsModule,
     FormsModule,
-    SelectButtonModule,
-    CalendarModule,
-    ListboxModule,
-    TableModule,
-    MaterialModule,
+    ViewComponentsModule,
     AppRoutingModule,
-    DropdownModule
 
   ],
   providers: [
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
+
+//Materials
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatMenuModule } from '@angular/material/menu';
+
+//primeNg
+import { ListboxModule } from 'primeng/listbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+
+//eCharts
+import { NgxEchartsModule } from 'ngx-echarts';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { TableModule } from 'primeng/table';
+
 
 @NgModule({
   declarations: [],
@@ -12,7 +25,12 @@ import { MatMenuModule } from '@angular/material/menu';
     MatToolbarModule,
     MatIconModule,
     MatTabsModule,
-    MatMenuModule
+    MatMenuModule,
+    ListboxModule,
+    DropdownModule,
+    CalendarModule,
+    SelectButtonModule,
+    TableModule
+
   ]
-})
-export class MaterialModule { }
+}) export class ViewComponentsModule { }
