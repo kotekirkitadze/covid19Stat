2c8725f984e4ea06d3c3d3a1c99108a3c3f6993f
kote1999 <kotekirkitadze@gmail.com>
2021-11-14 20:34:18 +0400

    calendar selector in progress

--- src/app/components/country-info-shell/country-info-shell.component.ts
+++ src/app/components/country-info-shell/country-info-shell.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { Country, CountryData, CountryDataAPI } from 'src/app/models/countryInfo';
 import { DataApiService } from 'src/app/services/data-api.service';
-import { mapCountryData } from '../../shared/utils/mapping.fn';
+import { mapCountryData, mapTimelineData } from '../../shared/utils/mapping.fn';
+import { handleDateFormat } from '../../shared/utils/handling.fn'
 
 @Component({
   selector: 'app-country-info-shell',
@@ -9,12 +11,45 @@ import { mapCountryData } from '../../shared/utils/mapping.fn';
   styleUrls: ['./country-info-shell.component.scss']
 })
 export class CountryInfoShellComponent implements OnInit {
+  private _rangeDates: Date[];
+  private handledDates: string[] = [];
+
+  set rangeDates(value: Date[]) {
+    this._rangeDates = value;
+    this.handleDataFormating(value);
+  }
+
+  get rangeDates() {
+    return this._rangeDates;
+  }
+
+  handleDataFormating(value: Date[]) {
+    this.handledDates = [];
+    if (this._rangeDates[1] !== null) {
+      this.handledDates.push(...value.map(handleDateFormat))
+    }
+  }
+
   countries: Country[];
   countriesInfo: CountryData[];
+  countryInfo: CountryData;
 
   _selectedCountry: Country;
   set selectedCountry(value) {
     this._selectedCountry = value;
+    this.http.getCountryDataByCode(value.code).pipe(
+      this.handleSelectedCountryMapping()
+    ).subscribe(console.log)
+
+  }
+
+  handleSelectedCountryMapping() {
+    return map<CountryDataAPI, CountryData>(el => {
+      return {
+        ...mapCountryData(el),
+        timeline: el.timeline.map(mapTimelineData)
+      }
+    })
   }
 
   get selectedCountry() {
@@ -30,10 +65,6 @@ export class CountryInfoShellComponent implements OnInit {
         this.countries = data.map(this.handleCountryMaping)
       }
     );
-
-    setTimeout(() => {
-      this.http.getCountryDataByCode('GE').subscribe(console.log)
-    }, 2000)
   }
 
   handleCountryMaping(el: CountryDataAPI): Country {
